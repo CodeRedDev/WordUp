98426d2b1787807e20f981b23dae87cdbfc7b06d
Michael Martel <codered.dev.germany@gmail.com>
2019-12-12 09:54:57 +0100

    Fixed gradles and test files

--- wordup/src/androidTest/java/de/codereddev/wordup/ExampleInstrumentedTest.kt
+++ wordup/src/androidTest/java/de/codereddev/wordup/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package de.codereddev.wordupexample
+package de.codereddev.wordup
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("de.codereddev.wordupexample", appContext.packageName)
+        assertEquals("de.codereddev.wordup.test", appContext.packageName)
     }
 }
