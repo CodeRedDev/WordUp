515d68197f0df9e0986e7d8150bf7d4e545f1c27
CodeRedDev <codered.dev.germany@gmail.com>
2023-03-26 22:10:34 +0200

    Updating dependencies
    Refactoring broken and deprecated parts

--- wordup-core/src/main/java/de/codereddev/wordup/player/LocalWordUpPlayer.kt
+++ wordup-core/src/main/java/de/codereddev/wordup/player/LocalWordUpPlayer.kt
@@ -2,15 +2,16 @@ package de.codereddev.wordup.player
 
 import android.content.Context
 import android.net.Uri
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultDataSource
 import de.codereddev.wordup.ErrorConstants
 import de.codereddev.wordup.database.Word
 
 class LocalWordUpPlayer(context: Context) : WordUpPlayer {
-    private val player = SimpleExoPlayer.Builder(context).build()
+    private val player = ExoPlayer.Builder(context).build()
 
     override fun play(context: Context, word: Word) {
         if (word.isNetworkResource)
@@ -38,12 +39,7 @@ class LocalWordUpPlayer(context: Context) : WordUpPlayer {
     }
 
     private fun buildMediaSource(context: Context, word: Word): MediaSource {
-        return ProgressiveMediaSource.Factory(
-            DefaultDataSourceFactory(context, USER_AGENT)
-        ).createMediaSource(Uri.parse("asset:///${word.path}"))
-    }
-
-    companion object {
-        const val USER_AGENT = "WordUpPlayer"
+        return ProgressiveMediaSource.Factory(DefaultDataSource.Factory(context))
+            .createMediaSource(MediaItem.fromUri(Uri.parse("asset:///${word.path}")))
     }
 }
